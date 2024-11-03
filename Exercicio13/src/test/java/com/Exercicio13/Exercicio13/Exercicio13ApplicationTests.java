cc66633f7b0e5544886be9d6815c3469827ab455
Isis Costa <isiscostabezbirolo@gmail.com>
2024-11-03 10:42:41 -0300

    EX13

---
+++ Exercicio13/src/test/java/com/Exercicio13/Exercicio13/Exercicio13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Exercicio13.Exercicio13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
